7c302233f70a3cde15aa6458120e4ff90b967bd6
sajid-ahmed16 <42862084+sajid-ahmed16@users.noreply.github.com>
2019-02-07 13:58:03 +0530

    Update alphabet.py

--- alphabet.py
+++ alphabet.py
@@ -1,5 +1,5 @@
-chr = input("")
+chr = raw_input("")
 if((chr>='a' and chr<= 'z') or (chr>='A' and chr<='Z')):
-    print(chr, "is an Alphabet")
+    print("is an Alphabet")
 else:
-    print(chr, "is not an Alphabet")
+    print("no")
