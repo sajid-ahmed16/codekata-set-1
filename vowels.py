b92ddd9101ee6a4907671597d687ee310ee16cd9
sajid-ahmed16 <42862084+sajid-ahmed16@users.noreply.github.com>
2019-02-07 13:46:09 +0530

    Update vowels.py

--- vowels.py
+++ vowels.py
@@ -1,7 +1,7 @@
-chr=raw_input("Enter the letter: ")
+chr=raw_input("")
 if(chr=='A' or chr=='a' or chr=='E' or chr=='e' or chr=='I' or chr=='i' or chr=='O' or chr=='o' or chr=='U' or chr=='u'):
     print("it is a Vowel")
 elif(chr=='B' or chr=='b' or chr=='C' or chr=='c' or chr=='D' or chr=='d' or chr=='F' or chr=='f' or chr=='G' or chr=='g' or chr=='H' or chr=='h' or chr=='J' or chr=='j' or chr=='K' or chr=='k' or chr=='L' or chr=='l' or chr=='M' or chr=='m' or chr=='N' or chr=='n' or chr=='P' or chr=='p' or chr=='Q' or chr=='q' or chr=='R' or chr=='r' or chr=='S' or chr=='s' or chr=='T' or chr=='t' or chr=='V' or chr=='v' or chr=='W' or chr=='w' or chr=='X' or chr=='x' or chr=='Y' or chr=='y' or chr=='Z' or chr=='z'):
     print("it is consonant")
 else:
-    print("invalid input")
+    print("invalid")
