fc6e12a17aebe744cb6cafd656f6b307ab0d88ab
sajid-ahmed16 <42862084+sajid-ahmed16@users.noreply.github.com>
2019-02-07 14:38:18 +0530

    Create largest.py

---
+++ largest.py
@@ -0,0 +1,10 @@
+A = float(raw_input(""))
+B = float(raw_input(""))
+C = float(raw_input(""))
+if (A > B) and (A > C):
+   largest = A
+elif(B > A) and (B > C):
+    largest = B
+else:
+    largest = c
+print(largest)
